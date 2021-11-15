5e68532bfa0d804fc14522a913ddb682457cf805
luoqing <luoqing@zhongbaounion.com>
2021-11-15 19:20:24 +0800

    update export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import ListQuery, { IDictItem } from './comps/list-query/list-query';
+
 import InfoBlock, { IContentField } from './comps/info-block/info-block';
 
+// ListQuery
+export type { IDictItem } from './comps/list-query/list-query';
+export { default as ListQuery} from './comps/list-query/list-query';
 
-export {
-  ListQuery,
-  InfoBlock,
-
-  IDictItem,
-  IContentField,
-}
+// InfoBlock
+export type { IContentField } from './comps/info-block/info-block';
+export { default as InfoBlock } from './comps/info-block/info-block';
